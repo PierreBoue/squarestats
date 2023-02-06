2c344482478f57ec6f36490ac4a6ce0f5a9848bf
Pierre Boué <pierre.boue@le-campus-numerique.fr>
2023-02-06 16:42:20 +0100

    Initial commit

---
+++ src/main/java/com/macaplix/squarestats/SquarestatsApplication.java
@@ -0,0 +1,13 @@
+package com.macaplix.squarestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquarestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquarestatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macaplix/squarestats/SquarestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macaplix.squarestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquarestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
